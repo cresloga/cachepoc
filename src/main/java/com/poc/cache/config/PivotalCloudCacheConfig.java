ac649070c2837b62857e1e726867d95beaae6951
Loganathan Murugesan <cresloga@gmail.com>
2017-11-28 12:30:28 +0530

    pcc changes

---
+++ src/main/java/com/poc/cache/config/PivotalCloudCacheConfig.java
@@ -0,0 +1,56 @@
+package com.poc.cache.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.ServiceConnectorConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
+import org.springframework.data.gemfire.support.GemfireCacheManager;
+import com.gemstone.gemfire.cache.Cache;
+import com.gemstone.gemfire.cache.client.ClientCache;
+import com.gemstone.gemfire.cache.client.ClientRegionShortcut;
+import io.pivotal.spring.cloud.service.gemfire.GemfireServiceConnectorConfig;
+
+@Configuration
+@EnableCaching
+@Profile({ "pcc", "gemfire" })
+public class PivotalCloudCacheConfig extends AbstractCloudConfig {
+
+	
+	private ServiceConnectorConfig createGemfireConnectorConfig() {
+		GemfireServiceConnectorConfig gemfireConfig = new GemfireServiceConnectorConfig();
+		return gemfireConfig;
+	}
+
+
+	@Bean
+	ClientCache clientCache() {
+		CloudFactory cloudFactory = new CloudFactory();
+		Cloud cloud = cloudFactory.getCloud();
+	    ClientCache cache = cloud.getServiceConnector("plans-cache", ClientCache.class, createGemfireConnectorConfig());
+		return cache;
+	}
+	
+	@Bean
+	 public CacheManager cacheManager(final Cache gemfireCache) {
+		 return new GemfireCacheManager() {{
+	            setCache(gemfireCache);
+	        }};
+	 }
+
+
+	@Bean
+	public ClientRegionFactoryBean<String, String> personRegion(ClientCache cache) {
+		ClientRegionFactoryBean<String,String> region = new ClientRegionFactoryBean<>();
+		region.setCache(cache);
+		region.setName("plans");
+		region.setShortcut(ClientRegionShortcut.PROXY);
+		return region;
+	}
+
+}
